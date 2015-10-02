b59efb5e0f0a08d11c7ed8368d339082bab1fa5f
Ross Jones <ross@servercode.co.uk>
2015-10-02 10:56:44 +0100

    Fix up the TTL rendering to be more complete

--- web/views/api_view.ex
+++ web/views/api_view.ex
@@ -1,4 +1,11 @@
 defmodule ApiServer.ApiView do
   use ApiServer.Web, :view
+  alias Poison, as: JSON
+
+  def hash_for_map(m) do
+    JSON.encode!(m)
+    |> :erlang.md5
+    |> Base.encode16(case: :lower)
+  end
 
 end
