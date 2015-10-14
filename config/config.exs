f7a5b2d07100a79875b62b1fa87b7eda5a1549f4
Ross Jones <ross@servercode.co.uk>
2015-10-14 18:16:04 +0100

    Update config

--- config/config.exs
+++ config/config.exs
@@ -7,7 +7,7 @@ use Mix.Config
 
 # Configures the endpoint
 config :api_server, ApiServer.Endpoint,
-  url: [host: "localhost", path: "/"],
+  url: [host: "localhost", path: "/data/api"],
   root: Path.dirname(__DIR__),
   secret_key_base:
     "p8zwrvtQEunaM7uXr0ngEjqahd9kSrGmAKxhL+y4ipG5A4DXAYbaBg/gQy6nB1xG",
