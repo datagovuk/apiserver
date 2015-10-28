cc43f6754b933420b2ac2867d90a25b7b2ac8df0
Ross Jones <ross@servercode.co.uk>
2015-10-28 14:40:44 +0000

    Fix controller test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule ApiServer.PageControllerTest do
 
   test "GET /" do
     conn = get conn(), "/"
-    assert html_response(conn, 200) =~ "DGU API"
+    assert html_response(conn, 200) =~ "Data.gov.uk - API Server"
   end
 end
