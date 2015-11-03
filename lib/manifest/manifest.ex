97d7863f07771292383b24eeff550f53e7788c10
Ross Jones <ross@servercode.co.uk>
2015-11-03 16:49:04 +0000

    Add a popover to show source dataset

--- lib/manifest/manifest.ex
+++ lib/manifest/manifest.ex
@@ -3,6 +3,6 @@ defmodule ApiServer.Manifest.Manifest do
   @moduledoc """
   Models a manifest that is loaded from a manifest JSON file.
   """
-  defstruct id: "", geo: false, theme: "", title: "", tablename: "", fields: [], choice_fields: [], queries: []
+  defstruct id: "", dataset: "", geo: false, theme: "", title: "", tablename: "", fields: [], choice_fields: [], queries: []
 
 end
